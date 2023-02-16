76deaf5e487df53312819a4d719757b79ed2dee2
Josef Vimr <pepino.vimr@seznam.cz>
2023-02-16 08:54:14 +0100

    Initial commit

---
+++ AnalogWatch/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AnalogWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WatchPB = new System.Windows.Forms.PictureBox();
+            this.SecondsTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.WatchPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WatchPB
+            // 
+            this.WatchPB.Location = new System.Drawing.Point(0, 0);
+            this.WatchPB.Name = "WatchPB";
+            this.WatchPB.Size = new System.Drawing.Size(400, 400);
+            this.WatchPB.TabIndex = 0;
+            this.WatchPB.TabStop = false;
+            this.WatchPB.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // SecondsTimer
+            // 
+            this.SecondsTimer.Interval = 1000;
+            this.SecondsTimer.Tick += new System.EventHandler(this.SecondsTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(402, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WatchPB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Deactivate += new System.EventHandler(this.Form1_SetNoneHandle);
+            this.VisibleChanged += new System.EventHandler(this.Form1_SetNoneHandle);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Enter += new System.EventHandler(this.Form1_SetNoneHandle);
+            this.Leave += new System.EventHandler(this.Form1_SetNoneHandle);
+            ((System.ComponentModel.ISupportInitialize)(this.WatchPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox WatchPB;
+        private System.Windows.Forms.Timer SecondsTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AnalogWatch/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace AnalogWatch
+{
+    public partial class Form1 : Form
+    {
+        Pen _secondsPen = new Pen(Color.Red);
+        Pen _minutePen = new Pen(Color.Gray);
+        Pen _hourPen = new Pen(Color.Black);
+        readonly Point _middlePB;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            g = WatchPB.CreateGraphics();
+            _middlePB = new Point(WatchPB.Width/2, WatchPB.Height/2);
+            SecondsTimer.Start();
+        }
+
+        private void SecondsTimer_Tick(object sender, EventArgs e)
+        {
+
+            g = WatchPB.CreateGraphics();
+            g.Clear(Color.White);
+            DrawWatch();
+            var now = DateTime.Now;
+            label1.Text = now.ToString();
+            MoveSecondsHand(now.Second);
+            MoveMinuteHand(now.Minute);
+            MoveHourHand(now.Hour);
+        }
+
+        private void MoveSecondsHand(int seconds)
+        {
+            g.TranslateTransform(_middlePB.X, _middlePB.Y);
+            g.RotateTransform(6 * seconds);
+            g.DrawLine(_secondsPen, 0, 0, 0, -160);
+            g.ResetTransform();
+        }
+
+        private void MoveMinuteHand(int minutes)
+        {
+            g.TranslateTransform(_middlePB.X, _middlePB.Y);
+            g.RotateTransform(6 * minutes);
+            g.DrawLine(_minutePen, 0, 0, 0, -125);
+            g.ResetTransform();
+        }
+
+        private void MoveHourHand(int hours)
+        {
+            g.TranslateTransform(_middlePB.X, _middlePB.Y);
+            g.RotateTransform(6 * hours);
+            g.DrawLine(_hourPen, 0, 0, 0, -90);
+            g.ResetTransform();
+        }
+        private void DrawWatch()
+        {
+            g.DrawEllipse(_hourPen, 0, 0, WatchPB.Width, WatchPB.Height);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (ActiveForm == null)
+                return;
+            if(ActiveForm.FormBorderStyle != FormBorderStyle.FixedDialog && ActiveForm.ContainsFocus)
+                ActiveForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            SecondsTimer_Tick(sender, e);
+        }
+
+        private void Form1_SetNoneHandle(object sender, EventArgs e)
+        {
+            if (ActiveForm == null)
+                return;
+            if (ActiveForm.FormBorderStyle != FormBorderStyle.None && ActiveForm.ContainsFocus)
+                ActiveForm.FormBorderStyle = FormBorderStyle.None;
+            SecondsTimer_Tick(sender, e);
+        }
+    }
+}
